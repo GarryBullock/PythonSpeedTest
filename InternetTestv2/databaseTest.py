984401e3bea46e99b1aad7585e7673bebeaa0c7a
gbullock <garry_bullock@intuit.com>
2016-03-22 22:01:19 -0600

    Update the databaseTest.py file to use datetime, cleaned up slightly.

--- InternetTestv2/databaseTest.py
+++ InternetTestv2/databaseTest.py
@@ -1,23 +1,24 @@
 import sqlite3 as sql
+from datetime import date, datetime
 
-db = sql.connect('data/sqlite3.db')
+db = sql.connect('data/sqlite3.sqlite')
 #db = sql.connect(':memory:')
-
-cursor = db.cursor()
-#cursor.execute('''CREATE TABLE tweets(id INTEGER PRIMARY KEY, down INTEGER,
-#                  up INTEGER, date TEXT)''')
-#db.commit()
 
 id = 1
 down = 5
 up = 10
-date = '21/03/2016,14:08'
-#cursor.execute('''INSERT INTO tweets(id, down, up, date)
-#                  VALUES(?,?,?,?)''', (id,down,up,date))
-#db.commit()
+date = datetime.now()
 
-cursor.execute('''SELECT id, down, up, date FROM tweets''')
+cursor = db.cursor()
+cursor.execute('''CREATE TABLE tweets(id INTEGER PRIMARY KEY, Down_Speed INTEGER,
+                  Up_Speed INTEGER, Log_Date DATE)''')
+db.commit()
+
+cursor.execute('''INSERT INTO tweets(id, Down_Speed, Up_Speed, Log_Date)
+                  VALUES(?,?,?,?)''', (id,down,up,date))
+db.commit()
+
+cursor.execute('''SELECT id, Down_Speed, Up_Speed, Log_Date FROM tweets''')
 for row in cursor:
     print(row)
 db.close()
-print("finished")
